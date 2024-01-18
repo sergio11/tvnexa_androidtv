9dfc4fde9aa16d7aaedf36300aeddfff8bf24875
Sergio Sánchez <sss4esob@gmail.com>
2024-01-18 21:40:57 +0100

    Implement OnboardingScreen design

--- app/src/main/java/com/dreamsoftware/tvnexa/ui/features/onboarding/OnboardingScreenContent.kt
+++ app/src/main/java/com/dreamsoftware/tvnexa/ui/features/onboarding/OnboardingScreenContent.kt
@@ -2,20 +2,26 @@
 
 package com.dreamsoftware.tvnexa.ui.features.onboarding
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.Text
 import com.dreamsoftware.tvnexa.R
 import com.dreamsoftware.tvnexa.ui.components.CommonButton
 import com.dreamsoftware.tvnexa.ui.components.CommonVideoBackground
@@ -30,10 +36,66 @@ fun OnboardingScreenContent(
         modifier = modifier.fillMaxSize()
     ) {
         OnboardingVideoBackground()
+        Row {
+            val commonModifier = Modifier
+                .fillMaxSize()
+                .weight(0.8f)
+            OnBoardingLogo(modifier = commonModifier)
+            OnboardingContentInfo(modifier = commonModifier)
+        }
         OnBoardingActions(
             onGoToSignIn = onGoToSignIn,
             onGoToSignUp = onGoToSignUp
         )
+    }
+}
+
+@Composable
+private fun OnBoardingLogo(modifier: Modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.tvnexa_logo_inverse),
+            contentDescription = null,
+            modifier = Modifier
+                .height(190.dp)
+                .align(Alignment.TopCenter)
+                .padding(top = 60.dp)
+        )
+    }
+}
+
+@Composable
+private fun OnboardingContentInfo(modifier: Modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "Explore the World of TVNexa!",
+                style = MaterialTheme.typography.headlineLarge,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "Discover a vast collection of television channels and enjoy an immersive viewing experience. Explore international channels with IPTV, bringing you content from around the globe.",
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(40.dp))
+            Text(
+                text = "Sign in or Sign up to unlock the full potential of TVNexa.",
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center,
+            )
+        }
     }
 }
 
@@ -43,7 +105,7 @@ private fun OnboardingVideoBackground() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.surface.copy(alpha = 0.7f))
+            .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.7f))
     )
 }
 
@@ -64,7 +126,8 @@ private fun BoxScope.OnBoardingActions(
         Spacer(modifier = Modifier.width(30.dp))
         CommonButton(
             onClick = onGoToSignUp,
-            text = "Sign Up"
+            text = "Sign Up",
+            inverseStyle = true
         )
     }
 }
