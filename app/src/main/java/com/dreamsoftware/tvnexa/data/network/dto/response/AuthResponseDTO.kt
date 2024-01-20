aefd6f7068fdea254463181c57d2a5205f8c6738
Sergio Sánchez <sss4esob@gmail.com>
2024-01-20 12:39:07 +0100

    Implement authentication retrofit service and related DTOs

---
+++ app/src/main/java/com/dreamsoftware/tvnexa/data/network/dto/response/AuthResponseDTO.kt
@@ -0,0 +1,18 @@
+package com.dreamsoftware.tvnexa.data.network.dto.response
+
+import com.squareup.moshi.Json
+
+/**
+ * Data Transfer Object (DTO) representing the structure of an authentication API response.
+ *
+ * @property token The authentication token associated with the user.
+ * @property user The user information as part of the authentication response.
+ *
+ * @param UserResponseDTO The type representing user information in the API response.
+ */
+data class AuthResponseDTO(
+    @field:Json(name = "token")
+    val token: String,
+    @field:Json(name = "user")
+    val user: UserResponseDTO,
+)
