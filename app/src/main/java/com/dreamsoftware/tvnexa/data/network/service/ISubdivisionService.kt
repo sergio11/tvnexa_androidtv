bfbaeee9a7e60663ca2732d931fe5bbfa3e8b1c5
Sergio Sánchez <sss4esob@gmail.com>
2024-01-22 21:34:31 +0100

    Implement retrofit services

---
+++ app/src/main/java/com/dreamsoftware/tvnexa/data/network/service/ISubdivisionService.kt
@@ -0,0 +1,19 @@
+package com.dreamsoftware.tvnexa.data.network.service
+
+import com.dreamsoftware.tvnexa.data.network.dto.response.ApiResponseDTO
+import com.dreamsoftware.tvnexa.data.network.dto.response.SubdivisionResponseDTO
+import retrofit2.http.GET
+
+/**
+ * Service interface for interacting with the Subdivision API.
+ */
+interface ISubdivisionService {
+
+    /**
+     * Fetches a list of all available subdivisions.
+     *
+     * @return A [ApiResponseDTO] containing a list of [SubdivisionResponseDTO].
+     */
+    @GET("/subdivisions/")
+    suspend fun all(): ApiResponseDTO<List<SubdivisionResponseDTO>>
+}
