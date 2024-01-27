17fbd6a6bec84da64006715ddfe941a1c4eb72fe
Sergio Sánchez <sss4esob@gmail.com>
2024-01-27 11:24:10 +0100

    Save AuthSession at SessionAware implementation when a new sign in has completed or when a session verification ocurred

--- app/src/main/java/com/dreamsoftware/tvnexa/domain/usecase/impl/VerifyUserSessionUseCase.kt
+++ app/src/main/java/com/dreamsoftware/tvnexa/domain/usecase/impl/VerifyUserSessionUseCase.kt
@@ -2,13 +2,17 @@ package com.dreamsoftware.tvnexa.domain.usecase.impl
 
 import com.dreamsoftware.tvnexa.domain.repository.IAuthRepository
 import com.dreamsoftware.tvnexa.domain.usecase.core.BaseUseCase
+import com.dreamsoftware.tvnexa.utils.ISessionAware
 
 class VerifyUserSessionUseCase(
-    private val authRepository: IAuthRepository
+    private val authRepository: IAuthRepository,
+    private val sessionAware: ISessionAware
 ): BaseUseCase<Boolean>() {
 
     override suspend fun onExecuted(): Boolean = runCatching {
-        authRepository.getSession()
+        authRepository.getSession().also {
+            sessionAware.session = it
+        }
         true
     }.getOrDefault(false)
 }
