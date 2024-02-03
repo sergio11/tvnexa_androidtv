74b13880618638d7595f7ccddbc52cbee04e5b7b
Sergio Sánchez <sss4esob@gmail.com>
2024-02-03 14:09:36 +0100

    Implement Search screen

---
+++ app/src/main/java/com/dreamsoftware/tvnexa/ui/components/CommonLoading.kt
@@ -0,0 +1,50 @@
+package com.dreamsoftware.tvnexa.ui.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.tv.material3.MaterialTheme
+
+@OptIn(ExperimentalTvMaterial3Api::class)
+@Composable
+fun CommonLoading(
+    modifier: Modifier = Modifier,
+    @StringRes text: Int,
+) {
+    with(MaterialTheme.colorScheme) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(surface),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CommonText(
+                type = CommonTextTypeEnum.TITLE_MEDIUM,
+                titleRes = text,
+                textColor = onSurface,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(40.dp)
+                    .padding(8.dp),
+                color = primary
+            )
+        }
+    }
+}
