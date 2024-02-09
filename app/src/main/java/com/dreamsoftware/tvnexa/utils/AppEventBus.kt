69e49ee605bcf80e65bf94edbbda78dcb6b0a996
Sergio Sánchez <sss4esob@gmail.com>
2024-02-09 21:41:54 +0100

    Implement App event bus and broadcast receiver to detect screen on and screen off events

---
+++ app/src/main/java/com/dreamsoftware/tvnexa/utils/AppEventBus.kt
@@ -0,0 +1,25 @@
+package com.dreamsoftware.tvnexa.utils
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+
+class AppEventBus {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Default)
+    private val _events = MutableSharedFlow<AppEvent>()
+    val events = _events.asSharedFlow()
+
+    fun send(event: AppEvent) {
+        coroutineScope.launch {
+            _events.emit(event)
+        }
+    }
+}
+
+sealed interface AppEvent {
+    data object GoToStandby : AppEvent
+    data object ComeFromStandby : AppEvent
+}
