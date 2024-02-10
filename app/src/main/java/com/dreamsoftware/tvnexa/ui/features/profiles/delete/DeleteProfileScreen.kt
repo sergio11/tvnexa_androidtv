468eac277b886f49c8644d60c89a18e4716fa3dd
Sergio Sánchez <sss4esob@gmail.com>
2024-02-10 13:35:37 +0100

    Implement delete profile screen

---
+++ app/src/main/java/com/dreamsoftware/tvnexa/ui/features/profiles/delete/DeleteProfileScreen.kt
@@ -0,0 +1,25 @@
+package com.dreamsoftware.tvnexa.ui.features.profiles.delete
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.dreamsoftware.tvnexa.ui.components.CommonScreen
+
+@Composable
+fun DeleteProfileScreen(
+    args: DeleteProfileScreenArgs,
+    viewModel: DeleteProfileViewModel = hiltViewModel(),
+    onBackPressed: () -> Unit
+) {
+    CommonScreen(
+        viewModel = viewModel,
+        onInit = { load(args.profileId) },
+        onBackPressed = onBackPressed,
+        onInitialUiState = { DeleteProfileUiState() },
+    ) { uiState ->
+        DeleteProfileScreenContent(
+            uiState = uiState,
+            onDeletePressed = ::onDeleteProfile,
+            onCancelPressed = onBackPressed
+        )
+    }
+}
---
+++ app/src/main/java/com/dreamsoftware/tvnexa/ui/features/profiles/delete/DeleteProfileScreenArgs.kt
@@ -0,0 +1,5 @@
+package com.dreamsoftware.tvnexa.ui.features.profiles.delete
+
+data class DeleteProfileScreenArgs(
+    val profileId: String
+)
