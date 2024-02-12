c939bb1051371cf3ea207aed1c07058ac9dc6bb0
Sergio Sánchez <sss4esob@gmail.com>
2024-02-12 20:56:27 +0100

    Implement settings section

--- app/src/main/java/com/dreamsoftware/tvnexa/ui/features/home/leftmenu/data/MenuData.kt
+++ app/src/main/java/com/dreamsoftware/tvnexa/ui/features/home/leftmenu/data/MenuData.kt
@@ -1,5 +1,6 @@
 package com.dreamsoftware.tvnexa.ui.features.home.leftmenu.data
 
+import com.dreamsoftware.tvnexa.R
 import com.dreamsoftware.tvnexa.ui.features.home.leftmenu.model.MenuItem
 import com.dreamsoftware.tvnexa.ui.navigation.Screens
 import compose.icons.LineAwesomeIcons
@@ -12,14 +13,33 @@ import compose.icons.lineawesomeicons.TvSolid
 object MenuData {
 
     val mainMenuItems = listOf(
-        MenuItem(Screens.Home.DEFAULT.path, "Home", LineAwesomeIcons.HomeSolid),
-        MenuItem(Screens.Home.Search.path, "Search", LineAwesomeIcons.SearchSolid),
-        MenuItem(Screens.Home.Guide.path, "Guide", LineAwesomeIcons.TvSolid),
-        MenuItem(Screens.Home.Favorites.path, "Favorites", LineAwesomeIcons.HeartSolid),
+        MenuItem(
+            id = Screens.Home.DEFAULT.path,
+            title = R.string.home_menu_main_item_text,
+            icon = LineAwesomeIcons.HomeSolid
+        ),
+        MenuItem(
+            id = Screens.Home.Search.path,
+            title = R.string.home_menu_search_item_text,
+            icon = LineAwesomeIcons.SearchSolid
+        ),
+        MenuItem(
+            id = Screens.Home.Guide.path,
+            title = R.string.home_menu_guide_item_text,
+            icon = LineAwesomeIcons.TvSolid
+        ),
+        MenuItem(
+            id = Screens.Home.Favorites.path,
+            title = R.string.home_menu_favorites_item_text,
+            icon = LineAwesomeIcons.HeartSolid
+        )
     )
 
     val secondaryMenuItems = listOf(
-        MenuItem(Screens.Home.Settings.DEFAULT.path, "Settings", LineAwesomeIcons.CogSolid)
+        MenuItem(
+            id = Screens.Home.Settings.DEFAULT.path,
+            title = R.string.home_menu_settings_item_text,
+            icon = LineAwesomeIcons.CogSolid
+        )
     )
-
 }
--- app/src/main/java/com/dreamsoftware/tvnexa/ui/features/settings/data/SettingsMenuData.kt
+++ app/src/main/java/com/dreamsoftware/tvnexa/ui/features/settings/data/SettingsMenuData.kt
@@ -1,13 +1,20 @@
 package com.dreamsoftware.tvnexa.ui.features.settings.data
 
+import com.dreamsoftware.tvnexa.R
 import com.dreamsoftware.tvnexa.ui.features.settings.model.SettingsItemMenu
 import com.dreamsoftware.tvnexa.ui.navigation.Screens
 
 object SettingsMenuData {
     val menu by lazy {
         listOf(
-            SettingsItemMenu(id = Screens.Home.Settings.Profile.path, title = "Profile"),
-            SettingsItemMenu(id = Screens.Home.Settings.AboutMe.path, title = "About Me"),
+            SettingsItemMenu(
+                id = Screens.Home.Settings.Profile.path,
+                title = R.string.setting_menu_profile_item_text
+            ),
+            SettingsItemMenu(
+                id = Screens.Home.Settings.AboutMe.path,
+                title = R.string.setting_menu_about_me_item_text
+            ),
         )
     }
 }
