432256a4bbfe8077d62c060f23fd11dbc9cf32ef
Sergio Sánchez <sss4esob@gmail.com>
2024-02-12 19:55:19 +0100

    Refactoring ChannelPreview using CommonChannelHeaderInfo

--- app/src/main/java/com/dreamsoftware/tvnexa/ui/components/ChannelPreview.kt
+++ app/src/main/java/com/dreamsoftware/tvnexa/ui/components/ChannelPreview.kt
@@ -7,23 +7,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.MaterialTheme
-import com.dreamsoftware.tvnexa.R
 import com.dreamsoftware.tvnexa.domain.model.SimpleChannelBO
 
 @Composable
@@ -47,37 +40,19 @@ fun ChannelPreview(
                 CommonVideoBackground(
                     videHlsResource = channel.streamUrl
                 )
-                Row (modifier = Modifier
-                    .fillMaxSize()
-                    .background(primaryContainer.copy(alpha = 0.6f))
-                    .padding(vertical = 20.dp, horizontal = 15.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.Bottom)  {
-                    Row(verticalAlignment = Alignment.Bottom) {
-                        ChannelLogo(size = 40.dp, logo = channel.logo)
-                        Spacer(modifier = Modifier.width(10.dp))
-                        Column {
-                            Spacer(modifier = Modifier.weight(1f))
-                            CommonText(
-                                titleText = channel.name,
-                                type = CommonTextTypeEnum.TITLE_LARGE,
-                                textBold = true,
-                                textColor = onPrimaryContainer
-                            )
-                            CommonText(
-                                titleText = channel.city,
-                                type = CommonTextTypeEnum.BODY_MEDIUM,
-                                textColor = onPrimaryContainer
-                            )
-                        }
-                    }
-                    if(channel.isNsfw == true) {
-                        CommonImageRes(
-                            modifier = Modifier.height(40.dp),
-                            imageRes = R.drawable.channel_nsfw_icon,
-                            tint = onPrimaryContainer
-                        )
-                    }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(primaryContainer.copy(alpha = 0.6f))
+                        .padding(vertical = 20.dp, horizontal = 15.dp),
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    CommonChannelHeaderInfo(
+                        name = channel.name,
+                        logo = channel.logo,
+                        city = channel.city,
+                        isNsfw = channel.isNsfw
+                    )
                 }
             }
         }
