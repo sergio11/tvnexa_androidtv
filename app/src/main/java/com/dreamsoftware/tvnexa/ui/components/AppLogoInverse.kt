f9a47ff715eef2c2a12ba3037226abf680ff4a06
Sergio Sánchez <sss4esob@gmail.com>
2024-02-12 19:00:58 +0100

    Implement several improvements at ChannelPreview component

--- app/src/main/java/com/dreamsoftware/tvnexa/ui/components/AppLogoInverse.kt
+++ app/src/main/java/com/dreamsoftware/tvnexa/ui/components/AppLogoInverse.kt
@@ -1,16 +1,13 @@
 package com.dreamsoftware.tvnexa.ui.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import com.dreamsoftware.tvnexa.R
 
 @Composable
 fun AppLogoInverse(modifier: Modifier) {
-    Image(
-        painter = painterResource(id = R.drawable.tvnexa_logo_inverse),
-        contentDescription = null,
-        modifier = modifier
+    CommonImageRes(
+        modifier = modifier,
+        imageRes = R.drawable.tvnexa_logo_inverse
     )
 }
