078c8fd7e6bfba160af9daa51f1ca4808a4a92bb
Sergio Sánchez <sss4esob@gmail.com>
2024-02-12 21:02:20 +0100

    improvement of project organization

--- app/src/main/java/com/dreamsoftware/tvnexa/ui/features/settings/model/SettingsItemMenu.kt
+++
@@ -1,8 +0,0 @@
-package com.dreamsoftware.tvnexa.ui.features.settings.model
-
-import androidx.annotation.StringRes
-
-data class SettingsItemMenu(
-    val id: String,
-    @StringRes val title: Int
-)
